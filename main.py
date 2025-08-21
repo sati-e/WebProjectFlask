f1c4e04ed40f3252ba1ed25edf87a593c3d47a4d
sati-e <satie.kc@gmail.com>
2025-08-21 15:58:33 -0300

    correcao

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from routes import *
 from forms import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "524fd"
+app.config['SECRET_KEY'] = ""
 
 #mysqldatabase
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:S@t1lanh4v.a0484@localhost:5252/users'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:00/users'
 #iniciar database
 db.init_app(app)
+
 
 #adicionar routes
 app.register_blueprint(bp)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5252)
+    with app.app_context():
+        db.create_all()
+        print("Tabelas criadas!")
+
+    app.run(debug=True, port=0)
